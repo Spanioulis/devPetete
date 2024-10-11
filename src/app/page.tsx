5bf164910cc23d8df03425b6c374aa365aa7687e
Spanioulis <pani77sps@gmail.com>
2024-10-11 10:14:18 +0200

    Install Lucide Icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
-import { MusicIcon } from "@/components/icons";
+import MusicIcon from "@/components/icons/MusicIcon";
+import { Book } from "lucide-react";
 
 export default function Home() {
 	return (
 		<main className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-10 font-[family-name:var(--font-geist-sans)]">
 			<header className="flex justify-between items-center w-full">
 				<div>
-					<h1 className="text-5xl font-bold text-crimson">📖 devPetete</h1>
+					<h1 className="text-5xl font-bold flex items-center gap-2">
+						<Book size={48} className="inline text-dodgerBlue" />{" "}
+						<span className="text-crimson">devPetete</span>
+					</h1>
 					<p className="text-2xl">Un sinfín devRecursos...</p>
 				</div>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
 				background: "var(--background)",
 				foreground: "var(--foreground)",
 				crimson: "#c34",
+				dodgerBlue: "#09f",
 			},
 		},
 	},
